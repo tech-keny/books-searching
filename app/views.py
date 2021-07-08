da9800327699ec0e364f05bd5af243a495f00cfe
tech-keny <chanken7@icloud.com>
2021-07-08 16:32:23 +0900

    途中（7月8日）

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import View
+from django.shortcuts import render
+from .forms import SearchForm
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
+
+        return render(request, 'app/index.html', {
+            'form': form
+        })
